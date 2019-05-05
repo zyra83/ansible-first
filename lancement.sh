4cb2a6c5f43e535cf88cb04458e8c03cab7c0dd6
Unknown <zyra83@hotmail.com>
2019-05-05 11:32:10 +0200

    plop

--- lancement.sh
+++ lancement.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-ansible-playbook -i hosts playbook.yaml
+# ask pass pour l'utlisateur root en ssh
+# --vault-password-file vault_pass.txt
+# --password-file vault_pass.txt
+
+#ansible-playbook -i hosts playbook.yml --vault-password-file vault_pass.txt --ask-pass
+ansible-playbook -i hosts playbook.yml --vault-password-file vault_pass.txt
